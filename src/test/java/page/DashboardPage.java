fc1861302a17fae08aa0fdfb361b61b39515e9f0
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2022-05-16 23:11:34 +0300

    Added .gitignore

---
+++ src/test/java/page/DashboardPage.java
@@ -0,0 +1,38 @@
+package page;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DashboardPage {
+    private SelenideElement heading = $(withText("Путешествие дня"));
+    private SelenideElement button1 = $(withText("Купить"));
+    private SelenideElement button2 = $(withText("Купить в кредит"));
+
+    private ElementsCollection cards = $$(".list__item");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+        button1.shouldBe(visible);
+        button2.shouldBe(visible);
+    }
+
+
+}
